24607788f2c492f4abccfb977fe73a0f2c906b68
Charlie McElfresh <cwmcelfresh@gmail.com>
2021-01-09 22:49:13 -0800

    Add EnforceAPIKataRequestContentType

--- cmd/server.go
+++ cmd/server.go
@@ -30,7 +30,8 @@ func init() {
 func serverCmdRunner() {
 	mux := http.NewServeMux()
 	rootHandler := http.HandlerFunc(rootHandler)
-	sharedMiddlewares := alice.New(middlewares.LogRequest)
+	sharedMiddlewares := alice.New(middlewares.
+		EnforceAPIKataRequestContentType, middlewares.LogRequest)
 	mux.Handle("/", sharedMiddlewares.Then(rootHandler))
 	logrus.Info(fmt.Sprintf("kata server running on %v",
 		config.Constants["SERVER_PORT"]))
